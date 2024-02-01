8d5e8c0a1467530a42e12be910cc701d97fd8705
rpotter741 <84155580+rpotter741@users.noreply.github.com>
2024-02-01 17:16:42 -0500

    Re-added logo

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
     mode: 'development',
@@ -14,6 +15,7 @@ module.exports = {
             title: "Settlement Management Sheet",
            
         }),
+        new FaviconsWebpackPlugin('./src/logo.png'),
     ],
     devtool: 'inline-source-map',
     devServer: {
