ab832b38b03b43516e8829ac312dca85119a0082
rpotter741 <robert.potter.741@gmail.com>
2024-04-16 16:09:28 -0400

    Update scratchpaper.js
    Working on a different class organization

--- scratchpaper.js
+++ scratchpaper.js
@@ -1,79 +1,52 @@
-function setStorage() {
-  localStorage.setItem("settlementData", JSON.stringify(settlement));
-  localStorage.setItem("survivalData", JSON.stringify(survival));
-  localStorage.setItem("safetyData", JSON.stringify(safety));
-  localStorage.setItem("economyData", JSON.stringify(economy));
-  localStorage.setItem("buildingData", JSON.stringify(buildings));
-  localStorage.setItem("projectArray", JSON.stringify(projectArray));
-  localStorage.setItem("deployedArray", JSON.stringify(deployedArray));
-  localStorage.setItem("playerNotes", JSON.stringify(userNotes));
-  localStorage.setItem("selectedUpgrades", JSON.stringify(selectedUpgrades));
-  localStorage.setItem("selectedLevels", JSON.stringify(selectedLevels));
-  localStorage.setItem(
-    "nonselectedUpgrades",
-    JSON.stringify(nonselectedUpgrades),
-  );
-  localStorage.setItem("projectCount", JSON.stringify(count));
-  localStorage.setItem("weekLog", JSON.stringify(weekLog));
-  localStorage.setItem("changeLog", JSON.stringify(changeLog));
-}
-
-function checkStorage() {
-  if (storageAvailable("localStorage")) {
-    if (!localStorage.getItem("settlementData")) {
-      setStorage();
-    } else {
-      loadStorage();
+class Settlement {
+  constructor(name, level = 3, type = None, upgrades = []) {
+    this.name = name; 
+    this.level = level;
+    this.settlementType = type;
+    this.upgrades = upgrades;
+    this.maxHealthBase = 0;
+    this.maxHealthBonus = 0;
+    this.currentHealth = 0; 
+    this.settlementPoints = 0;
+    this.survivalBonus = 0;
+    this.foodBonus = 0;
+    this.foodCurrent = 0;
+    this.foodMax = 0;
+    this.suppliesBonus = 0;
+    this.suppliesCurrent = 0;
+    this.suppliesMax = 0;
+    this.medicineBonus = 0;
+    this.medicineCurrent = 0;
+    this.medicineMax = 0;
+    this.safetyBonus = 0;
+    this.defenseBonus = 0;
+    this.defenseCurrent = 0;
+    this.defenseMax = 0;
+    this.intelBonus = 0;
+    this.intelCurrent = 0;
+    this.intelMax = 0;
+    this.garrisonBonus = 0;
+    this.garrisonCurrent = 0;
+    this.garrisonMax = 0; 
+    this.economyBonus = 0;
+    this.tradeBonus = 0;
+    this.tradeCurrent = 0;
+    this.tradeMax = 0; 
+    this.productivityBonus = 0;
+    this.productivityCurrent = 0;
+    this.productivityMax = 0;
+    this.taxVault = 0;
+    this.wells = 0;
+    this.walls = 0;
+    this.barracks = 0;
+    this.granary = 0;
+    this.market = 0;
+    this.network = 0;
+    this.union = 0;
+    this.school = 0;
+    this.commerce = 0;
     }
-  } else {
-    alert("fuck no");
-  }
-}
-
-function loadStorage() {
-  let settlementData = JSON.parse(localStorage.getItem("settlementData"));
-  settlement = settlementData;
-
-  let survivalData = JSON.parse(localStorage.getItem("survivalData"));
-  survival = survivalData;
-
-  let safetyData = JSON.parse(localStorage.getItem("safetyData"));
-  safety = safetyData;
-
-  let economyData = JSON.parse(localStorage.getItem("economyData"));
-  economy = economyData;
-
-  let buildingData = JSON.parse(localStorage.getItem("buildingData"));
-  buildings = buildingData;
-
-  let projectData = JSON.parse(localStorage.getItem("projectArray"));
-  projectArray = projectData;
-
-  let deployedData = JSON.parse(localStorage.getItem("deployedArray"));
-  deployedArray = deployedData;
-
-  let notesData = JSON.parse(localStorage.getItem("playerNotes"));
-  userNotes = notesData;
-
-  let selectedData = JSON.parse(localStorage.getItem("selectedUpgrades"));
-  selectedUpgrades = selectedData;
-
-  let selectedLevelsData = JSON.parse(localStorage.getItem("selectedLevels"));
-  selectedLevels = selectedLevelsData;
-
-  let nonselectedData = JSON.parse(localStorage.getItem("nonselectedUpgrades"));
-  nonselectedUpgrades = nonselectedData;
-
-  let countData = JSON.parse(localStorage.getItem("projectCount"));
-  count = countData;
-
-  let weekData = JSON.parse(localStorage.getItem("weekLog"));
-  weekLog = weekData;
-
-  let changeData = JSON.parse(localStorage.getItem("changeLog"));
-  changeLog = changeData;
-
-  console.log(changeData);
-}
-
-export { checkStorage, setStorage };
+    
+    function initialize() {
+      
+    }
